ac4e7a30cdcb6349d1bbcb2393f07c3b6967524d
Daniel Barclay <barclaysd@me.com>
2018-12-01 18:18:13 +0000

    Added perfromance timings

--- day01-chronal-calibration/freqency.js
+++ day01-chronal-calibration/freqency.js
@@ -18,11 +18,14 @@ const input = fs.readFileSync(path.resolve(__dirname, 'input.txt'), {
     encoding: 'UTF-8'
 });
 
+let part1Start = performance.now();
+
 // get numbers into an array
 const numbers = input.trim().split('\n').map(number => parseInt(number));
 const sumOfNumbers = numbers.reduce(reducer);
 
 console.log(sumOfNumbers);
+console.log('Runtime - Part 1:', performance.now() - part1Start);
 
 // Answer: 531
 
@@ -35,6 +38,7 @@ Part 2
 let combinedFrequency = 0;
 let frequencies = {};
 
+let part2Start = performance.now();
 while (true) {
     // makes an object of all values - checks in try loop if a key-pair value already exists
     // hence a duplicate is found
@@ -54,5 +58,7 @@ while (true) {
     }
 }
 console.log(combinedFrequency);
+let part2End = performance.now();
+console.log('Runtime - Part 2:', part2End - part2Start);
 
 // Answer: 76787
